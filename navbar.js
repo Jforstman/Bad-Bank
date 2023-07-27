4bd77efdba0c980607c68098115c014ffa256a76
JForstman <justlovinglife73@gmail.com>
2023-07-27 15:27:46 -0500

    updated navbar to tabs

--- navbar.js
+++ navbar.js
@@ -1,30 +1,27 @@
 function NavBar(){
   return(
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">BadBank</a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
+      <a className="navbar-brand">BadBank</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="#/CreateAccount/">Create Account</a>
+        <ul className="nav nav-tabs">
+          <li className="nav-item active">
+            <a className="nav-link text-white" href="#">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/login/">Login</a>
+            <a className="nav-link text-white" href="#/createaccount/">Create Account</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/deposit/">Deposit</a>
+            <a className="nav-link text-white" href="#/deposit/">Deposit</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/withdraw/">Withdraw</a>
+            <a className="nav-link text-white" href="#/withdraw/">Withdraw</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#/balance/">Balance</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/alldata/">AllData</a>
+            <a className="nav-link text-white" href="#/alldata/">All Data</a>
           </li>          
         </ul>
       </div>
@@ -32,3 +29,7 @@ function NavBar(){
     </>
   );
 }
+
+//<li className="nav-item">
+//<a className="nav-link" href="#/balance/">Balance</a>
+//</li>
