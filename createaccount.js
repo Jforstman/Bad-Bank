5faeb91415f6b983ccf58168325fcafed347834d
JForstman <justlovinglife73@gmail.com>
2023-07-28 17:56:25 -0500

    changed bootstrap card

--- createaccount.js
+++ createaccount.js
@@ -40,13 +40,12 @@ function CreateAccount(){
     <Card
       bgcolor="primary"
       header={<>
-          <span className="placeholder col-12 bg-dark"></span>
-          <h1>Create Account</h1>
-          <span className="placeholder col-12 bg-dark"></span>
+          <h1 className="bg-danger text-white">Create Account</h1>
       </>}
       status={status}
       body={show ? (  
-              <>
+              <div className="card text-white">
+                <div className="card-body bg-danger">
               Name<br/>
               <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
               Email address<br/>
@@ -54,7 +53,8 @@ function CreateAccount(){
               Password<br/>
               <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
               <button disabled={!name && !email && !password} type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
-              </>
+              </div>
+            </div>
             ):(
               <>
               <h5>Success</h5>
