1e20b301e10818cbbe3921888d34c421ccb68bfd
JForstman <justlovinglife73@gmail.com>
2023-07-28 17:52:26 -0500

    Added hover text to navbar

--- navbar.js
+++ navbar.js
@@ -9,19 +9,19 @@ function NavBar(){
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="nav nav-tabs">
           <li className="nav-item active">
-            <a className="nav-link text-white" href="#">Home</a>
+            <a className="nav-link text-white" data-toggle="tooltip" data-placement="bottom" title="Home / Landing Page" href="#">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link text-white" href="#/createaccount/">Create Account</a>
+            <a className="nav-link text-white" data-toggle="tooltip" data-placement="bottom" title="Create a new account here!" href="#/createaccount/">Create Account</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link text-white" href="#/deposit/">Deposit</a>
+            <a className="nav-link text-white" data-toggle="tooltip" data-placement="bottom" title="Make a deposit to your account" href="#/deposit/">Deposit</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link text-white" href="#/withdraw/">Withdraw</a>
+            <a className="nav-link text-white" data-toggle="tooltip" data-placement="bottom" title="Make a withdrawal from your account" href="#/withdraw/">Withdraw</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link text-white" href="#/alldata/">All Data</a>
+            <a className="nav-link text-white" data-toggle="tooltip" data-placement="bottom" title="All user account data" href="#/alldata/">All Data</a>
           </li>          
         </ul>
       </div>
